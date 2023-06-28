3e142cbe8491fb2c4d130fd14e603d3ff5e70230
jubayer <jubayermd327@gmail.com>
2023-06-28 08:54:01 +0600

    step 1

--- auctionSite/products/models.py
+++ auctionSite/products/models.py
@@ -9,3 +9,12 @@ class User(models.Model):
 
     def __str__(self):
         return self.email
+
+
+class AuctionItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product_name = models.CharField(max_length=255)
+    product_description = models.TextField()
+    product_photo = models.ImageField(upload_to='auction_items')
+    minimum_bid_price = models.DecimalField(max_digits=10, decimal_places=2)
+    auction_end_datetime = models.DateTimeField()
